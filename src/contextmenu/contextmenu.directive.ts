da8bf7b937deb40ee2d01a876583c27bb65edcec
Dennis Sänger <mail@dennis.io>
2017-04-26 10:02:49 +0200

    feat(touch/click): enable menu onClick for touch devices

--- src/contextmenu/contextmenu.directive.ts
+++ src/contextmenu/contextmenu.directive.ts
@@ -5,6 +5,11 @@ import {
   HostListener
 } from '@angular/core';
 
+/*global DocumentTouch:false */
+declare var DocumentTouch: any;
+const isTouch = !!(('ontouchstart' in window) ||
+      (window as any).DocumentTouch && document instanceof DocumentTouch);
+
 @Directive({
   selector: '[contextmenu]',
   providers: []
@@ -23,4 +28,12 @@ export class ContextmenuDirective {
     ev.stopPropagation();
     ev.preventDefault();
   }
+
+  @HostListener('click', ['$event'])
+  onClick(ev: any) {
+    if (isTouch) {
+      return this.onContextmenu(ev);
+    }
+  }
+
 }
